29bb73583ca2de3486b7ba2965bccb764170b019
Rafael <rao9787@gmail.com>
2022-10-08 12:34:32 -0400

    add adjoint derivation

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,7 @@ makedocs(sitename="Photoacoustic imaging in Julia",
          authors="Rafael Orozco and Mathias Louboutin",
          pages = Any[
              "Home" => "index.md",
-             "Equations" => "derivations.md",
+             "Theory" => "derivations.md",
              "Tutorials" => ["LSQR.md", "LearnedPrior.md"]
          ])
 
